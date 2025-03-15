be9abd526c67ee806e0ceb3aee1f50fdf7d282d3
Andreas Klos <aklos@outlook.de>
2025-03-15 10:57:08 +0100

    chore: update open-api generator script and replace openapi-spec with openapi.yaml

--- open-api-generator.sh
+++ open-api-generator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --user $(id -u):$(id -g) --rm -v $PWD:/local openapitools/openapi-generator-cli@sha256:b35aee2d0f6ffadadcdad9d8fc3c46e8d48360c20b5731a5f47c809d51f67a04 generate -i openapi-spec.yaml -g python-fastapi -o /local --additional-properties=packageName=rag_core_api,generateSourceCodeOnly=True
+docker run --user $(id -u):$(id -g) --rm -v $PWD:/local openapitools/openapi-generator-cli@sha256:b35aee2d0f6ffadadcdad9d8fc3c46e8d48360c20b5731a5f47c809d51f67a04 generate -i /local/openapi.yaml -g python-fastapi -o /local --additional-properties=packageName=langchain_ocr,generateSourceCodeOnly=True
