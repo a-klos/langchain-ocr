9569b14309da265665eb35e33b22cdb5d9609453
Andreas Klos <andreas.klos21@gmail.com>
2025-03-30 23:09:45 +0200

    refactor: extract library and replace dependency injection package (#5)
    * feat: implement dependency injection container for OCR application
    
    * refactor: put ocr logic into a separate package
    
    * refactor: update dependency injection and improve file conversion logic
    
    * fix: correct formatting in OllamaSettings model field
    
    * chore: add initial test configuration and clean up unused test files
    
    * style: clean up whitespace and formatting across multiple files
    
    * chore: update pythonpath in pyproject.toml to include src directory
    
    * chore: update flake8 exclude patterns to include tests directory
    
    * docs: enhance docstring for ConvertFile2Markdown class and aconvert2markdown method
    
    * docs: enhance docstrings for ConvertImageEndpoint and ConvertPdfEndpoint classes and their methods
    
    * docs: enhance docstrings across multiple modules for clarity and consistency
    
    * test: remove obsolete ocr_api_test.py file
    
    ---------
    
    Co-authored-by: Andreas Klos <aklos@outlook.de>

---
+++ api/src/langchain_ocr/impl/api_endpoints/convert_image_endpoint.py
@@ -0,0 +1,50 @@
+"""Module for the implementation of the ConvertImageEndpoint class."""
+
+from fastapi import UploadFile
+import inject
+import io
+from PIL import Image
+from langchain_ocr_lib.impl.converter.image_converter import Image2MarkdownConverter
+
+from langchain_ocr.api_endpoints.convert_base import ConvertFile2Markdown
+
+
+class ConvertImageEndpoint(ConvertFile2Markdown):
+    """Converts an image to markdown format.
+
+    Attributes
+    ----------
+    _converter : Image2MarkdownConverter
+        An instance of the Image2MarkdownConverter class, injected using the `inject` library.
+        This converter is responsible for performing the actual image to markdown conversion.
+    """
+
+    _converter: Image2MarkdownConverter = inject.attr("ImageConverter")
+
+    async def aconvert2markdown(self, body: UploadFile) -> str:
+        """Asynchronously converts an uploaded image to markdown format.
+
+        Parameters
+        ----------
+        body : UploadFile
+            The uploaded image file.
+
+        Returns
+        -------
+        str
+            The markdown representation of the image.
+
+        Raises
+        ------
+        ValueError
+            If the uploaded file is not a valid image or if the conversion fails.
+        """
+        file = await body.read()
+
+        try:
+            image = Image.open(io.BytesIO(file))
+
+        except Exception as e:
+            raise ValueError("Image corrupted or unsupported file type: %s" % e)
+
+        return await self._converter.aconvert2markdown(image=image, filename=None)
