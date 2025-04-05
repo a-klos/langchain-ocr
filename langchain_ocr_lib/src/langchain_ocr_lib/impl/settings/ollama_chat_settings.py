5435f0a8d032f221355554e7b57970d65b209008
Andreas Klos <aklos@outlook.de>
2025-04-05 23:16:27 +0200

    feat: update Dockerfile to install poppler-utils and change API port to 8080; modify default model in OllamaSettings to gemma3

--- langchain_ocr_lib/src/langchain_ocr_lib/impl/settings/ollama_chat_settings.py
+++ langchain_ocr_lib/src/langchain_ocr_lib/impl/settings/ollama_chat_settings.py
@@ -35,7 +35,7 @@ class OllamaSettings(BaseSettings):
         env_prefix = "OLLAMA_"
         case_sensitive = False
 
-    model: str = Field(default="x/llama3.2-vision:11b-instruct-fp16")
+    model: str = Field(default="gemma3")
     base_url: str = Field(default="http://localhost:11434")
     top_k: int = Field(default=0, title="LLM Top K")
     top_p: float = Field(default=0, title="LLM Top P")
