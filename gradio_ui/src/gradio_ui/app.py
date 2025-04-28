0cfa668d49e0fce2475c41bce2afe63deeb3183c
Andreas Klos <aklos@outlook.de>
2025-04-28 17:24:40 +0200

    feat: enhance pyproject.toml with additional development dependencies and configuration for flake8, black, isort, and pylint; refactor app and converter modules for improved clarity and functionality

--- gradio_ui/src/gradio_ui/app.py
+++ gradio_ui/src/gradio_ui/app.py
@@ -1,3 +1,5 @@
+"""Module to create a Gradio UI for LangChain OCR."""
+
 import gradio as gr
 from gradio_pdf import PDF
 from pathlib import Path
@@ -5,12 +7,13 @@ from pathlib import Path
 from gradio_ui.converter import Converter
 
 
-def perform_ocr(doc: str) -> str:
+def _perform_ocr(doc: str) -> str:
     converter = Converter()
     return converter.convert(filename=doc)
 
 
 def main():
+    """Run the Gradio app."""
     with gr.Blocks(
         title="Langchain-OCR Playground",
         theme=gr.themes.Soft(primary_hue="blue", secondary_hue="cyan"),
@@ -19,15 +22,20 @@ def main():
         gr.Markdown(
             """
             Upload a PDF file and extract structured text using LLM-powered OCR.
-            
+
             The application will convert your document to markdown format.
             """
         )
         dir_ = Path(__file__).parent
         gr.Interface(
-            perform_ocr,
+            _perform_ocr,
             [PDF(label="PDF Document")],
-            gr.Markdown(label="Output (Markdown)",min_height=270, container=True, show_copy_button=True),
+            gr.Markdown(
+                label="Output (Markdown)",
+                min_height=270,
+                container=True,
+                show_copy_button=True,
+            ),
             examples=[
                 [str(dir_ / "examples" / "invoice.pdf")],
                 [str(dir_ / "examples" / "invoice-hw.pdf")],
--- gradio_ui/src/gradio_ui/converter.py
+++ gradio_ui/src/gradio_ui/converter.py
@@ -1,3 +1,5 @@
+"""Module for converting PDF files to Markdown format using a converter class."""
+
 import inject
 
 from langchain_ocr_lib.di_config import configure_di
@@ -9,7 +11,22 @@ configure_di()  # This sets up the dependency injection
 
 
 class Converter:
+    """Class to convert PDF files to Markdown format."""
+
     _converter: Pdf2MarkdownConverter = inject.attr(PdfConverterKey)
 
     def convert(self, filename: str) -> str:
+        """
+        Convert a PDF file to Markdown format.
+
+        Parameters
+        ----------
+        filename : str
+            Path to the PDF file.
+
+        Returns
+        -------
+        str
+            Markdown formatted content.
+        """
         return self._converter.convert2markdown(filename=filename)
